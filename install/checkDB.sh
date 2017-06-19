eab3378883769694fea0aebf2b6bebbefe65dd2c
bobby <bwuphan@gmail.com>
2017-06-18 21:32:22 -0700

    First commit

---
+++ install/checkDB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+				cd /usr/local/mysql/bin
+				./mysql -u root -pbpha1299 --execute="SHOW Databases" mysql
+				
---
+++ install/createDB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+				cd /usr/local/mysql/bin
+				./mysql -uroot -pbpha1299 -e "CREATE DATABASE learncfinaweek"
+				
